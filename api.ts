48a24527e04fa5a4a187d746c92cf6c7c68aa30c
Yiming Li <yimingli.cs@gmail.com>
2022-04-06 12:30:15 +0800

    better error msg for new items

--- api.ts
+++ api.ts
@@ -14,77 +14,6 @@ declare global {
 }
 
 
-const login = async (page: puppeteer.Page, __username: string, __password: string) => {
-  let errMsg = await page.evaluate((__username: string, __password: string): string | undefined => {
-    try {
-      (document.getElementById('username') as HTMLInputElement)!.value = __username;
-      (document.getElementById('password') as HTMLInputElement)!.value = __password;
-      (document.querySelector('.login-button > button') as HTMLButtonElement).click()
-    } catch (err) {
-      return (err as Error)?.message
-    }
-  }, __username, __password);
-
-  await page.waitForTimeout(3000)
-
-  errMsg ??= await page.evaluate((): string | undefined => {
-    const errMsg = document.getElementById('msg')?.textContent
-    console.log('errMsg', errMsg)
-    if (errMsg) {
-      return errMsg
-    }
-  })
-
-  if (errMsg) throw new Error(`登录失败: ${errMsg}`)
-  console.log(`${__username} 登陆成功！`)
-  await page.waitForTimeout(3000)
-}
-
-const submit = async (page: puppeteer.Page, dev: boolean) => {
-  let errMsg = await page.evaluate((): string | undefined => {
-    try {
-      const { vm } = window
-      // 是否在校
-      vm.setSfzx('1')
-      // 密切接触
-      vm.info.sfymqjczrj = 0
-      // 本人承诺
-      vm.info.sfqrxxss = 1
-      if (vm.oldInfo?.geo_api_info == null) {
-        return `vm.oldInfo.geo_api_info 为空，请先手动打一次卡，确保有地理位置信息存在。`
-      }
-      vm.locatComplete(JSON.parse(vm.oldInfo.geo_api_info))
-      vm.save()
-    } catch (err) {
-      return (err as Error)?.message
-    }
-  })
-  await page.waitForTimeout(1000)
-  errMsg ??= await page.evaluate(() => {
-    let popup = document.getElementById('wapat')
-    if (popup) {
-      if (getComputedStyle(popup).display !== 'none') {
-        return document.querySelector('.wapat-title')?.textContent ?? undefined
-      }
-    }
-  })
-  let oldInfo = await page.evaluate(() => (window.vm.oldInfo as JSONObject))
-  if (errMsg) throw new Error(`打卡提交失败：${errMsg}
-
-${dev ? `你前一次打卡的信息为：
-
-${JSON.stringify(oldInfo, null, 2)}
-
-请检查之前的提交是否缺少了什么信息，如有必要请手动打一次卡。
-
-如果遇到问题，请附上脱敏后的 oldInfo 前往 GitHub 提交 issue: https://github.com/zju-health-report/action/issues/new
-` : '将环境变量 NODE_ENV 设置为 development 可以获得 oldInfo 的详细信息，请参考官方文档: https://github.com/zju-health-report/action#报告问题'}
-
-`)
-  console.log(`打卡成功！`)
-  await page.waitForTimeout(3000)
-}
-
 export async function runZjuHealthReport(username?: string, password?: string) {
   if (!username) {
     throw new Error('请配置环境变量 username，详情请阅读项目 README.md: https://github.com/zju-health-report/action')
@@ -103,6 +32,76 @@ export async function runZjuHealthReport(username?: string, password?: string) {
   await page.goto('https://healthreport.zju.edu.cn/ncov/wap/default/index', {
     waitUntil: 'networkidle2',
   });
+
+  const chalk = new (await import('chalk')).Chalk({
+    level: 3
+  })
+
+  const login = async (page: puppeteer.Page, __username: string, __password: string) => {
+    let errMsg = await page.evaluate((__username: string, __password: string): string | undefined => {
+      try {
+        (document.getElementById('username') as HTMLInputElement)!.value = __username;
+        (document.getElementById('password') as HTMLInputElement)!.value = __password;
+        (document.querySelector('.login-button > button') as HTMLButtonElement).click()
+      } catch (err) {
+        return (err as Error)?.message
+      }
+    }, __username, __password);
+
+    await page.waitForTimeout(3000)
+
+    errMsg ??= await page.evaluate((): string | undefined => {
+      const errMsg = document.getElementById('msg')?.textContent
+      if (errMsg) {
+        return errMsg
+      }
+    })
+
+    if (errMsg) throw new Error(`登录失败，网页报错为: ${chalk.red(errMsg)}`)
+    console.log(`${__username} ${chalk.green('登陆成功！')}\n`)
+    await page.waitForTimeout(3000)
+  }
+
+  const submit = async (page: puppeteer.Page, dev: boolean) => {
+    let errMsg = await page.evaluate((): string | undefined => {
+      try {
+        const { vm } = window
+        Object.assign(vm.info, vm.oldInfo)
+        vm.confirm()
+      } catch (err) {
+        return (err as Error)?.message
+      }
+    })
+    await page.waitForTimeout(1000)
+    errMsg ??= await page.evaluate(() => {
+      let popup = document.getElementById('wapat')
+      if (popup) {
+        if (getComputedStyle(popup).display !== 'none') {
+          return document.querySelector('.wapat-title')?.textContent ?? undefined
+        }
+      }
+    })
+    let oldInfo = await page.evaluate(() => (window.vm.oldInfo as JSONObject))
+    let errorGuide = `常见错误：
+    1. 今天已经打过卡了，可以忽略此报错。
+    2. 表单可能新增了内容，请检查之前的提交是否缺少了什么信息，如有必要请手动打一次卡。`
+    if (errMsg) throw new Error(`打卡提交失败，网页报错为：${chalk.red(errMsg)}
+  ${dev ? `你前一次打卡的信息为：
+
+  ${JSON.stringify(oldInfo, null, 2)}
+
+  ${errorGuide}
+
+  如果遇到问题，请附上脱敏后的 oldInfo 前往 GitHub 提交 issue: https://github.com/zju-health-report/action/issues/new
+  ` : `
+  ${errorGuide}
+
+  将环境变量 NODE_ENV 设置为 development 可以获得 oldInfo 的详细信息，请参考官方文档: https://github.com/zju-health-report/action#报告问题`}
+`)
+    console.log(chalk.green(`打卡成功！`))
+    await page.waitForTimeout(3000)
+  }
+
 
   try {
     console.log(banner)
--- cli.ts
+++ cli.ts
@@ -4,7 +4,15 @@ import dotenv from 'dotenv'
 dotenv.config()
 import { runZjuHealthReport } from "./api";
 
+async function run() {
+  const username = process.env.username
+  const password = process.env.password
 
-const username = process.env.username
-const password = process.env.password
-runZjuHealthReport(username, password)
+  try {
+    await runZjuHealthReport(username, password)
+  } catch (error) {
+    console.log((error as Error)?.message ?? '未知错误');
+  }
+}
+
+run()
